aa8851742c485186a662910458969ca1a6c623f6
vmladenovic <mladenovic.info@gmail.com>
2025-09-23 09:05:23 +0200

    TabPanel fix

--- src/components/tabs/tab-panel.tsx
+++ src/components/tabs/tab-panel.tsx
@@ -1,11 +1,11 @@
 import type {PropsWithChildren} from 'react';
 
-interface Props extends PropsWithChildren {
+interface Props {
     isVisible: boolean;
 }
 
 // A simple TabPanel component that conditionally renders its children based on the isVisible prop.
-export function TabPanel({isVisible, children}: Props) {
+export function TabPanel({isVisible, children}: PropsWithChildren<Props>) {
     if (!isVisible) {
         return null;
     }
